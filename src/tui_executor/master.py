883eee8661bb537851883e0b7cb78143bdd92d6e
Rik Huygen <rik.huygen@kuleuven.be>
2024-11-11 17:03:00 +0100

    🚧 Added action to start a kernel

--- src/tui_executor/master.py
+++ src/tui_executor/master.py
@@ -1,39 +1,48 @@
+from __future__ import annotations
+
 import importlib
-import textwrap
 from typing import List
 
 from textual import on
+from textual import work
 from textual.app import ComposeResult
+from textual.binding import Binding
 from textual.containers import Grid
 from textual.containers import Horizontal
 from textual.containers import Vertical
-from textual.events import Event
 from textual.screen import Screen
-from textual.widgets import Button
 from textual.widgets import Footer
 from textual.widgets import Header
-from textual.widgets import RichLog
 from textual.widgets import TabbedContent
 
+from .dialogs import YesNoDialog
 from .funcpars import get_parameters
 from .functions import run_function
+from .kernel import MyKernel
+from .kernel import start_kernel
 from .modules import get_ui_subpackages
+from .panels import ConsoleOutput
 from .panels import PackagePanel
 from .tasks import TaskButton
 from .utils import extract_var_name_args_and_kwargs
 
 
 class MasterScreen(Screen):
+
+    BINDINGS = [
+        Binding("k", "start_kernel", "Start a kernel"),
+    ]
+
     def __init__(self, module_path_list: List[str]):
         super().__init__()
 
         self.module_path_list = module_path_list
         self.tabs = {}
 
+        self.kernel: MyKernel | None = None
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-
-        self.log.info(f"MasterScreen: {self.module_path_list = }")
 
         yield Header()
         yield Footer()
@@ -47,32 +56,61 @@ class MasterScreen(Screen):
 
             with Vertical():
                 yield Grid(name="Arguments", id="arguments-panel")
-                yield RichLog(max_lines=200, markup=True, id="console-log")
+                yield ConsoleOutput(max_lines=200, markup=True, id="console-log")
 
     def on_mount(self) -> None:
-        self.query_one("#console-log", RichLog).write(textwrap.dedent(
-            """\
-            2024-10-16T12:14:56 [blue]tui_executor[/] INFO The TUI Executor App is ready to launch
-            2024-10-16T12:14:56 [blue]tui_executor[/] INFO Composition established
-            2024-10-16T12:14:57 [blue]tui_executor[/] INFO Mounting ...
-            """
-        ))
+        self.query_one("#console-log", ConsoleOutput).write_log_info("The TUI Executor App is ready to launch.")
+        self.query_one("#console-log", ConsoleOutput).write_log_info("Composition established.")
+        self.query_one("#console-log", ConsoleOutput).write_log_info("Mounting ...")
+
         self.query_one("#arguments-panel", Grid).border_title = "Arguments"
 
+    @work()
+    async def action_start_kernel(self, force: bool = False) -> None:
+
+        name = "python3"  # get the kernel name from a widget or from a the command panel
+
+        # Starting the kernel will need a proper PYTHONPATH for importing the packages
+
+        if force:
+            if self.kernel is not None:
+                self.query_one("#console-log", ConsoleOutput).write_log_info("Shutdown kernel...")
+                self.kernel.shutdown()
+            self.query_one("#console-log", ConsoleOutput).write_log_info("Starting kernel (forced)...")
+            self.kernel = start_kernel(name)
+            return
+
+        if self.kernel is None:
+            self.query_one("#console-log", ConsoleOutput).write_log_info("Starting kernel...")
+            self.kernel = start_kernel(name)
+        else:
+            if await self.app.push_screen_wait(
+                YesNoDialog(title="Restart Jupyter kernel",
+                            question="A kernel is running, should a new kernel be started?")
+            ):
+                self.query_one("#console-log", ConsoleOutput).write_log_info("Shutdown kernel...")
+                self.kernel.shutdown()
+                self.query_one("#console-log", ConsoleOutput).write_log_info("Starting new kernel...")
+                self.kernel = start_kernel(name)
+
     @on(TaskButton.Pressed)
-    def run_task(self, event: Event):
+    def run_task(self, event: TaskButton.Pressed):
         button: TaskButton = event.button
 
         self.log.info(f"TaskButton: {event = }, {type(event) = }, {button = }")
 
         if button.immediate_run():
-            self.log.info("Task is run immediately when pressed.")
+            self.query_one(ConsoleOutput).write_log_info(
+                f"Task {button.function.__name__} is run immediately when pressed."
+            )
 
             ui_pars = get_parameters(button.function)
             args, kwargs = extract_var_name_args_and_kwargs(ui_pars)
-            run_function(button.function, args, kwargs, button.function.__ui_runnable__)
+            run_function(button.function, args, kwargs)
 
-        self.log.info("Preparing the ArgumentsPanel..")
+            return
+
+        self.query_one(ConsoleOutput).write_log_info("Preparing the ArgumentsPanel..")
 
         # Prepare and show the ArgumentsPanel. The function will be executed when the Run button is pressed in the
         # ArgumentsPanel.
