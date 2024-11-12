8340246513afd67ccab4385675496ac2c31fed47
Rik Huygen <rik.huygen@kuleuven.be>
2024-11-12 08:38:48 +0100

    ✅ added some more immediate_run functions with different return types and default arguments

--- tests/tasks/shared/unit_tests/immediate_run.py
+++ tests/tasks/shared/unit_tests/immediate_run.py
@@ -1,14 +1,26 @@
 from pathlib import Path
 
-from tui_executor.exec import exec_ui
+from tui_executor.exec import exec_task
 
 HERE = Path(__file__).parent.resolve()
 ICON_PATH = HERE / "../../../../src/tui_executor/icons/"
 
 
-@exec_ui(immediate_run=True, icons=(ICON_PATH / "stop.svg", ICON_PATH / "stop.svg"))
-def stop_hexapod():
+@exec_task(immediate_run=True, icons=(ICON_PATH / "stop.svg", ICON_PATH / "stop.svg"))
+def stop_immediately():
     import rich
     from rich.panel import Panel
 
     rich.print(Panel("Pressed the [red]STOP[/] button!"))
+
+
+@exec_task(immediate_run=True)
+def return_arguments_as_string(a: int = 42, b: float = 3.14, c: str = "default") -> str:
+
+    return f"{a = }, {b = }, {c = }"
+
+
+@exec_task(immediate_run=True)
+def return_arguments_as_tuple(a: int = 42, b: float = 3.14, c: str = "default") -> tuple:
+
+    return a, b, c
