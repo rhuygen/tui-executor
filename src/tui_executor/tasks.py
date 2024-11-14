237fbb0ffd61eda4438bd0619982b850a5ff3fbe
Rik Huygen <rik.huygen@kuleuven.be>
2024-11-14 11:18:50 +0100

    improvements to the TaskButton

--- src/tui_executor/tasks.py
+++ src/tui_executor/tasks.py
@@ -3,6 +3,7 @@ __all__ = [
     "TaskKind",
 ]
 
+import sys
 import textwrap
 from enum import IntEnum
 from typing import Callable
@@ -10,7 +11,7 @@ from typing import Callable
 from textual.widgets import Button
 
 
-def get_task_button_label(func: Callable, label: str = None) -> str:
+def get_task_button_label(func: Callable, label: str = None, triangles: bool = False) -> str:
     """
     Determine the label for the task button associated to the given function. If the task was
     given a display name, that will have precedence over the given label. If both are None,
@@ -19,14 +20,23 @@ def get_task_button_label(func: Callable, label: str = None) -> str:
     Args:
         func: the function that is associated with the button
         label: an optional label that will have precedence over the function name as a label
+        triangles: Put ▶ function ◀ around the button label
 
     Returns:
         A string representing the label for the button.
     """
     if hasattr(func, "__ui_display_name__") and func.__ui_display_name__:
-        return func.__ui_display_name__
+        name = func.__ui_display_name__
     else:
-        return label or func.__name__
+        name = label or func.__name__
+
+    # The following line will put the display_name within triangles: ▶︎ name ◀︎
+    # when the immediate_run flag is True.
+
+    if triangles and hasattr(func, "__ui_immediate_run__") and func.__ui_immediate_run__:
+        name = f"\u25B6 {name} \u25C0"
+
+    return name
 
 
 class TaskKind(IntEnum):
@@ -44,15 +54,52 @@ class TaskButton(Button):
     def __init__(self, name: str, function: Callable):
         super().__init__(name=name)
 
-        self.label = get_task_button_label(function)  # noqa
-        self.function = function
+        self.label = get_task_button_label(function, triangles=True)  # noqa
+        self._function = function
 
         if function.__doc__:
             self.tooltip = textwrap.dedent(function.__doc__)
 
     def on_mount(self):
-        if self.immediate_run():
+        if self.immediate_run:
             self.add_class("immediate_run")
 
-    def immediate_run(self):
-        return self.function.__ui_immediate_run__
+    @property
+    def immediate_run(self) -> bool:
+        if hasattr(self._function, "__ui_immediate_run__"):
+            return self._function.__ui_immediate_run__
+        return False
+
+    @property
+    def function(self) -> Callable:
+        return self._function
+
+    @property
+    def function_name(self) -> str:
+        return self._function.__name__
+
+    @property
+    def function_display_name(self) -> str:
+        if hasattr(self._function, "__ui_display_name__") and self._function.__ui_display_name__:
+            return self._function.__ui_display_name__
+        return ""
+
+    @property
+    def module_name(self) -> str:
+        """Returns the name of the module where the function resides."""
+        if hasattr(self._function, "__ui_module__"):
+            return self._function.__ui_module__
+        else:
+            return ""
+
+    @property
+    def module_display_name(self) -> str:
+        try:
+            try:
+                # This attribute is given to the function when copying the function object
+                # to be used in a different TAB with another display name
+                return self._function.__ui_module_display_name__
+            except AttributeError:
+                return sys.modules[self._function.__ui_module__].UI_MODULE_DISPLAY_NAME
+        except (AttributeError, KeyError):
+            return self.module_name.rsplit(".", 1)[-1]
